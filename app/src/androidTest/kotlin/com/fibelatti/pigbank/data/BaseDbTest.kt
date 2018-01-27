802efa86ce8a1e6a08b77764aa7b3e4045dc172a
Filipe Belatti <fibelatti@gmail.com>
2018-01-27 20:29:20 +0000

    Creates base database test

---
+++ app/src/androidTest/kotlin/com/fibelatti/pigbank/data/BaseDbTest.kt
@@ -0,0 +1,23 @@
+package com.fibelatti.pigbank.data
+
+import android.arch.persistence.room.Room
+import android.support.test.InstrumentationRegistry
+import com.fibelatti.pigbank.data.localdatasource.AppDatabase
+import com.fibelatti.raffler.BaseTest
+import org.junit.After
+import org.junit.Before
+
+abstract class BaseDbTest : BaseTest() {
+    protected lateinit var appDatabase: AppDatabase
+
+    @Before
+    fun initDb() {
+        appDatabase = Room.inMemoryDatabaseBuilder(InstrumentationRegistry.getContext(),
+            AppDatabase::class.java).build()
+    }
+
+    @After
+    fun closeDb() {
+        appDatabase.close()
+    }
+}
