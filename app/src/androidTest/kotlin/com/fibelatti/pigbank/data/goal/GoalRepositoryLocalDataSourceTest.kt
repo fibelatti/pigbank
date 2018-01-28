81dd8c476a86de8fec02cf56f8ec8b0818470806
Filipe Belatti <fibelatti@gmail.com>
2018-01-28 01:45:16 +0000

    Adds unit test to update goals

--- app/src/androidTest/kotlin/com/fibelatti/pigbank/data/goal/GoalRepositoryLocalDataSourceTest.kt
+++ app/src/androidTest/kotlin/com/fibelatti/pigbank/data/goal/GoalRepositoryLocalDataSourceTest.kt
@@ -20,10 +20,69 @@ class GoalRepositoryLocalDataSourceTest : BaseDbTest() {
     private val goalSavingsAfter = 300F
     private val goalDeadline = Date(1546214400) // 2018/12/31
 
-    private val savingsId = 10L
-    private val savingsAmount = 300F
-    private val savingsDate = Date(1517356800) // 2018/01/31
+    private val firstSavingsId = 10L
+    private val firstSavingsAmount = 300F
+    private val firstSavingsDate = Date(1517356800) // 2018/01/31
+
+    private val secondSavingsId = 20L
+    private val secondSavingsAmount = 200F
+    private val secondSavingsDate = Date(1517356800) // 2018/01/31
     //endregion
+
+    @Test
+    fun updateGoal() {
+        // Arrange
+        val firstTestObserver = TestObserver<GoalWithSavings>()
+
+        insertGoal(goalSavingsAfter)
+        insertSavings()
+
+        appDatabase.getGoalRepository()
+            .getGoalById(goalId)
+            .subscribeOn(testSchedulerProvider.io())
+            .observeOn(testSchedulerProvider.mainThread())
+            .subscribe(firstTestObserver)
+
+        assertSingleOnCompleteWithNoErrors(firstTestObserver)
+        assertGoal(firstTestObserver, goalSavingsAfter)
+        assertSavings(firstTestObserver)
+
+        // Act
+        val savings = arrayOf(
+            Savings(firstSavingsId, goalId, firstSavingsAmount, firstSavingsDate),
+            Savings(secondSavingsId, goalId, secondSavingsAmount, secondSavingsDate)
+        )
+
+        appDatabase.getSavingsRepository().saveSavings(*savings)
+
+        // Assert
+        val secondTestObserver = TestObserver<GoalWithSavings>()
+        appDatabase.getGoalRepository()
+            .getGoalById(goalId)
+            .subscribeOn(testSchedulerProvider.io())
+            .observeOn(testSchedulerProvider.mainThread())
+            .subscribe(secondTestObserver)
+
+        assertSingleOnCompleteWithNoErrors(secondTestObserver)
+        val result = secondTestObserver.values()[0]
+        with(result) {
+            assertEquals(goalId, goal.id)
+            assertEquals(goalDescription, goal.description)
+            assertEquals(goalCost, goal.cost)
+            assertEquals(goalSavingsAfter, goal.savings)
+            assertEquals(goalDeadline, goal.deadline)
+
+            assertEquals(2, result.savings.size)
+
+            assertEquals(firstSavingsId, result.savings[0].id)
+            assertEquals(firstSavingsAmount, result.savings[0].amount)
+            assertEquals(firstSavingsDate, result.savings[0].date)
+
+            assertEquals(secondSavingsId, result.savings[1].id)
+            assertEquals(secondSavingsAmount, result.savings[1].amount)
+            assertEquals(secondSavingsDate, result.savings[1].date)
+        }
+    }
 
     @Test
     fun getGroupById() {
@@ -104,7 +163,7 @@ class GoalRepositoryLocalDataSourceTest : BaseDbTest() {
 
     private fun createGoal(savings: Float = goalSavingsBefore) = Goal(goalId, goalDescription, goalCost, savings, goalDeadline)
 
-    private fun createSavings() = Savings(savingsId, goalId, savingsAmount, savingsDate)
+    private fun createSavings() = Savings(firstSavingsId, goalId, firstSavingsAmount, firstSavingsDate)
 
     private fun insertGoal(savings: Float = goalSavingsBefore) {
         appDatabase.getGoalRepository().saveGoal(createGoal(savings))
@@ -130,9 +189,9 @@ class GoalRepositoryLocalDataSourceTest : BaseDbTest() {
 
         assertTrue(result.savings.isNotEmpty())
         with(result.savings[0]) {
-            assertEquals(savingsId, id)
-            assertEquals(savingsAmount, amount)
-            assertEquals(savingsDate, date)
+            assertEquals(firstSavingsId, id)
+            assertEquals(firstSavingsAmount, amount)
+            assertEquals(firstSavingsDate, date)
         }
     }
 }
