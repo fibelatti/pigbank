fd93933854e9c6501ab00f9e506ce675a000c24c
Filipe Belatti <fibelatti@gmail.com>
2018-01-28 01:57:19 +0000

    Get and Delete Goal use cases

---
+++ app/src/main/kotlin/com/fibelatti/pigbank/domain/goal/GetGoalsUseCase.kt
@@ -0,0 +1,22 @@
+package com.fibelatti.pigbank.domain.goal
+
+import com.fibelatti.pigbank.data.localdatasource.AppDatabase
+import io.reactivex.Single
+import javax.inject.Inject
+import com.fibelatti.pigbank.data.goal.Goal as DataModel
+import com.fibelatti.pigbank.presentation.models.Goal as PresentationModel
+
+class GetGoalsUseCase @Inject constructor(private val database: AppDatabase) {
+    fun getAllGoals(): Single<List<PresentationModel>> =
+        database.getGoalRepository()
+            .getAllGoals()
+            .onErrorReturn { emptyList() }
+            .flattenAsObservable<DataModel> { list -> list }
+            .map { GoalMapper.toPresentationModel(goal = it) }
+            .toList()
+
+    fun getGoalById(id: Long): Single<PresentationModel> =
+        database.getGoalRepository()
+            .getGoalById(id)
+            .map { GoalMapper.toPresentationModel(goalWithSavings = it) }
+}
