57d13b35f4e2910acf26b9ccf2b02eff3e9c9feb
Filipe Belatti <fibelatti@gmail.com>
2018-01-28 23:52:25 +0000

    Creates activities and add them to the manifest

---
+++ app/src/main/kotlin/com/fibelatti/pigbank/presentation/goals/GoalsActivity.kt
@@ -0,0 +1,61 @@
+package com.fibelatti.pigbank.presentation.goals
+
+import android.content.Context
+import android.os.Bundle
+import com.fibelatti.pigbank.presentation.base.BaseActivity
+import com.fibelatti.pigbank.presentation.base.BaseIntentBuilder
+
+class GoalsActivity :
+    BaseActivity() {
+    //region Companion objects and interfaces
+    companion object {
+        val TAG: String = GoalsActivity::class.java.simpleName
+    }
+    //endregion
+
+    //region Public properties
+    //endregion
+
+    //region Private properties
+    //endregion
+
+    //region Override properties
+    //endregion
+
+    //region Override Lifecycle methods
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        TODO("setContentView")
+        TODO("YourApplicationFile.instance.plusYourComponent(activity = this)")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        TODO("presenter.bind(this)")
+    }
+
+    override fun onPause() {
+        TODO("presenter.unbind()")
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        TODO("YourApplicationFile.instance.clearYourComponent()")
+        super.onDestroy()
+    }
+
+    //endregion
+
+    //region Override methods
+    override fun handleError(errorMessage: String?) {
+        TODO("not implemented")
+    }
+    //endregion
+
+    //region Public methods
+    //endregion
+
+    //region Private methods
+    //endregion
+    class IntentBuilder(context: Context) : BaseIntentBuilder<GoalsActivity>(context, GoalsActivity::class.java)
+}
