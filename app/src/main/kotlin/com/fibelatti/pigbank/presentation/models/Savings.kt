757cab1d5bd7c9ca36dcab90e4b9850a93006da8
Filipe Belatti <fibelatti@gmail.com>
2018-01-28 01:44:59 +0000

    Creates Goal presentation models

---
+++ app/src/main/kotlin/com/fibelatti/pigbank/presentation/models/Savings.kt
@@ -0,0 +1,36 @@
+package com.fibelatti.pigbank.presentation.models
+
+import android.os.Parcel
+import android.os.Parcelable
+import com.fibelatti.pigbank.presentation.common.createParcel
+import com.fibelatti.pigbank.presentation.common.readDate
+import com.fibelatti.pigbank.presentation.common.writeDate
+import java.util.Date
+
+data class Savings(
+    val id: Long,
+    val goalId: Long,
+    val amount: Float,
+    val date: Date
+) : Parcelable {
+    constructor(source: Parcel) : this(
+        source.readLong(),
+        source.readLong(),
+        source.readFloat(),
+        source.readDate()
+    )
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
+        writeLong(id)
+        writeLong(goalId)
+        writeFloat(amount)
+        writeDate(date)
+    }
+
+    companion object {
+        @JvmField
+        val CREATOR = createParcel { Savings(it) }
+    }
+}
