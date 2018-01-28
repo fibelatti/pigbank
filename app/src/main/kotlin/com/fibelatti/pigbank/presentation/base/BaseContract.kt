0eb8ca064d75776bdf73062e61be63e2bab0bf57
Filipe Belatti <fibelatti@gmail.com>
2018-01-28 23:24:50 +0000

    Creates base and common classes

---
+++ app/src/main/kotlin/com/fibelatti/pigbank/presentation/base/BaseContract.kt
@@ -0,0 +1,13 @@
+package com.fibelatti.pigbank.presentation.base
+
+interface BaseContract {
+    interface View {
+        fun handleError(errorMessage: String?)
+    }
+
+    interface Presenter<in V : View> {
+        fun bind(view: V)
+
+        fun unbind()
+    }
+}
---
+++ app/src/main/kotlin/com/fibelatti/pigbank/presentation/base/BasePresenter.kt
@@ -0,0 +1,53 @@
+package com.fibelatti.pigbank.presentation.base
+
+import android.support.annotation.CallSuper
+import com.fibelatti.pigbank.presentation.common.SchedulerProvider
+import io.reactivex.Completable
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
+abstract class BasePresenter<in V : BaseContract.View>(protected val schedulerProvider: SchedulerProvider) :
+    BaseContract.Presenter<V> {
+
+    private lateinit var viewDisposables: CompositeDisposable
+
+    //region protected Observable, Single & Completable extensions
+    protected fun <T> Observable<T>.subscribeUntilDetached(onNext: (T) -> Unit):
+        Disposable = subscribe(onNext).apply { disposeOnDetach(disposable = this) }
+
+    protected fun <T> Observable<T>.subscribeUntilDetached(onNext: (T) -> Unit,
+                                                           onError: (Throwable) -> Unit):
+        Disposable = subscribe(onNext, onError).apply { disposeOnDetach(disposable = this) }
+
+    protected fun <T> Observable<T>.subscribeUntilDetached(onNext: (T) -> Unit,
+                                                           onError: (Throwable) -> Unit,
+                                                           onComplete: () -> Unit):
+        Disposable = subscribe(onNext, onError, onComplete).apply { disposeOnDetach(disposable = this) }
+
+    protected fun <T> Single<T>.subscribeUntilDetached(onComplete: (T) -> Unit):
+        Disposable = subscribe(onComplete).apply { disposeOnDetach(disposable = this) }
+
+    protected fun <T> Single<T>.subscribeUntilDetached(onComplete: (T) -> Unit,
+                                                       onError: (Throwable) -> Unit):
+        Disposable = subscribe(onComplete, onError).apply { disposeOnDetach(disposable = this) }
+
+    protected fun Completable.subscribeUntilDetached(onComplete: () -> Unit,
+                                                     onError: (Throwable) -> Unit):
+        Disposable = subscribe(onComplete, onError).apply { disposeOnDetach(disposable = this) }
+    //endregion
+
+    @CallSuper
+    override fun bind(view: V) {
+        viewDisposables = CompositeDisposable()
+    }
+
+    override fun unbind() {
+        viewDisposables.dispose()
+    }
+
+    private fun disposeOnDetach(disposable: Disposable) {
+        viewDisposables.add(disposable)
+    }
+}
