c1265cf243c8d7690e2a4591992305c59a048635
Filipe Belatti <fibelatti@gmail.com>
2018-01-29 08:30:30 +0000

    Tweaks the GoalsContract to make each interface smaller

--- app/src/main/kotlin/com/fibelatti/pigbank/presentation/goals/GoalsContract.kt
+++ app/src/main/kotlin/com/fibelatti/pigbank/presentation/goals/GoalsContract.kt
@@ -7,25 +7,29 @@ import com.fibelatti.pigbank.presentation.models.Goal
 interface GoalsContract {
     interface Presenter : BaseContract.Presenter<View>
 
-    interface View : BaseContract.View {
-        //region Produces
+    interface View : BaseContract.View, Producer, Consumer
+
+    interface Producer {
         fun preferencesClicked(): ObservableView<Unit>
 
         fun addGoalClicked(): ObservableView<Unit>
 
         fun goalClicked(): ObservableView<Goal>
 
-        fun addSavingsClicked(): ObservableView<Pair<Goal, Float>>
-        //endregion
+        fun addSavingsClicked(): ObservableView<Goal>
 
-        //region Consumes
+        fun addSavingsToGoal(): ObservableView<Pair<Goal, Float>>
+    }
+
+    interface Consumer {
         fun goToPreferences()
 
         fun createGoal()
 
         fun openGoal(goal: Goal)
 
+        fun showAddSavingsDialog(goal: Goal)
+
         fun updateGoals(goals: List<Goal>)
-        //endregion
     }
 }
