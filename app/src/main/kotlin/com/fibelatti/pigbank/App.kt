c856d5f625e9a5d653b61e8ff9aa8a9909626df3
Filipe Belatti <fibelatti@gmail.com>
2018-01-29 00:07:32 +0000

    Creates dependency injection classes for the features

--- app/src/main/kotlin/com/fibelatti/pigbank/App.kt
+++ app/src/main/kotlin/com/fibelatti/pigbank/App.kt
@@ -1,14 +1,73 @@
 package com.fibelatti.pigbank
 
 import android.app.Application
+import android.support.v4.app.FragmentActivity
+import com.fibelatti.pigbank.di.component.AppComponent
+import com.fibelatti.pigbank.di.component.DaggerAppComponent
+import com.fibelatti.pigbank.di.component.GoalDetailComponent
+import com.fibelatti.pigbank.di.component.GoalsComponent
+import com.fibelatti.pigbank.di.component.PreferencesComponent
+import com.fibelatti.pigbank.di.module.AppModule
+import com.fibelatti.pigbank.di.module.GoalDetailModule
+import com.fibelatti.pigbank.di.module.GoalsModule
+import com.fibelatti.pigbank.di.module.PreferencesModule
 
 class App : Application() {
     companion object {
         lateinit var instance: App
+
+        lateinit var appComponent: AppComponent
     }
+
+    private var goalsComponent: GoalsComponent? = null
+    private var goalDetailComponent: GoalDetailComponent? = null
+    private var preferencesComponent: PreferencesComponent? = null
 
     override fun onCreate() {
         super.onCreate()
         instance = this
+        buildComponents()
+    }
+
+    private fun buildComponents() {
+        appComponent = DaggerAppComponent.builder()
+            .appModule(AppModule(this))
+            .build()
+    }
+
+    fun instantiateGoalsComponent(activity: FragmentActivity): GoalsComponent? {
+        if (goalsComponent == null) {
+            goalsComponent = appComponent.instantiateComponent(GoalsModule(activity))
+        }
+
+        return goalsComponent
+    }
+
+    fun releaseGoalsComponent() {
+        goalsComponent = null
+    }
+
+    fun instantiateGoalDetailComponent(activity: FragmentActivity): GoalDetailComponent? {
+        if (goalDetailComponent == null) {
+            goalDetailComponent = appComponent.instantiateComponent(GoalDetailModule(activity))
+        }
+
+        return goalDetailComponent
+    }
+
+    fun releaseGoalDetailComponent() {
+        goalDetailComponent = null
+    }
+
+    fun instantiatePreferencesComponent(activity: FragmentActivity): PreferencesComponent? {
+        if (preferencesComponent == null) {
+            preferencesComponent = appComponent.instantiateComponent(PreferencesModule(activity))
+        }
+
+        return preferencesComponent
+    }
+
+    fun releasePreferencesComponent() {
+        preferencesComponent = null
     }
 }
--- app/src/main/kotlin/com/fibelatti/pigbank/di/component/AppComponent.kt
+++ app/src/main/kotlin/com/fibelatti/pigbank/di/component/AppComponent.kt
@@ -1,6 +1,9 @@
 package com.fibelatti.pigbank.di.component
 
 import com.fibelatti.pigbank.di.module.AppModule
+import com.fibelatti.pigbank.di.module.GoalDetailModule
+import com.fibelatti.pigbank.di.module.GoalsModule
+import com.fibelatti.pigbank.di.module.PreferencesModule
 import com.fibelatti.pigbank.di.scope.AppScope
 import dagger.Component
 
@@ -8,4 +11,10 @@ import dagger.Component
     modules = [AppModule::class]
 )
 @AppScope
-interface AppComponent
+interface AppComponent {
+    fun instantiateComponent(goalsModule: GoalsModule): GoalsComponent
+
+    fun instantiateComponent(goalDetailModule: GoalDetailModule): GoalDetailComponent
+
+    fun instantiateComponent(preferencesModule: PreferencesModule): PreferencesComponent
+}
