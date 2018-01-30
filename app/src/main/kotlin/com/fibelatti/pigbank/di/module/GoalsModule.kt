fae76185c6c360388448307e139af5354d4823a8
Filipe Belatti <fibelatti@gmail.com>
2018-01-30 19:49:07 +0000

    Removes unnecessary binding

--- app/src/main/kotlin/com/fibelatti/pigbank/di/module/GoalsModule.kt
+++ app/src/main/kotlin/com/fibelatti/pigbank/di/module/GoalsModule.kt
@@ -1,27 +1,18 @@
 package com.fibelatti.pigbank.di.module
 
-import com.fibelatti.pigbank.di.module.GoalsModule.Binder
 import com.fibelatti.pigbank.domain.goal.GetGoalsUseCase
 import com.fibelatti.pigbank.domain.goal.SaveForGoalUseCase
 import com.fibelatti.pigbank.presentation.common.SchedulerProvider
-import com.fibelatti.pigbank.presentation.goals.GoalsActivity
 import com.fibelatti.pigbank.presentation.goals.GoalsContract
 import com.fibelatti.pigbank.presentation.goals.GoalsPresenter
 import com.fibelatti.pigbank.presentation.goals.adapter.GoalsAdapter
 import com.fibelatti.pigbank.presentation.goals.adapter.GoalsDelegateAdapter
 import com.fibelatti.pigbank.presentation.goals.adapter.LoadingDelegateAdapter
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 
-@Module(includes = [Binder::class])
+@Module
 class GoalsModule {
-    @Module
-    interface Binder {
-        @Binds
-        fun provideGoalsActivity(goalsActivity: GoalsActivity): GoalsActivity
-    }
-
     @Provides
     fun provideGoalsPresenter(schedulerProvider: SchedulerProvider,
                               getGoalsUseCase: GetGoalsUseCase,
