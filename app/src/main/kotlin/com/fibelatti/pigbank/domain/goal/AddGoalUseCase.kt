ffa396e15ba9d0b933b8fb4ed26871589bc0fffe
Filipe Belatti <fibelatti@gmail.com>
2018-02-03 12:10:56 +0000

    Fixes AddGoalUseCase implementation

--- app/src/main/kotlin/com/fibelatti/pigbank/domain/goal/AddGoalUseCase.kt
+++ app/src/main/kotlin/com/fibelatti/pigbank/domain/goal/AddGoalUseCase.kt
@@ -3,24 +3,27 @@ package com.fibelatti.pigbank.domain.goal
 import com.fibelatti.pigbank.common.sumByFloat
 import com.fibelatti.pigbank.data.goal.Savings
 import com.fibelatti.pigbank.data.localdatasource.AppDatabase
+import com.fibelatti.pigbank.data.localdatasource.DATABASE_GENERIC_ERROR_MESSAGE
 import com.fibelatti.pigbank.presentation.models.Goal
-import io.reactivex.Completable
+import io.reactivex.Single
 import javax.inject.Inject
 
 class AddGoalUseCase @Inject constructor(private val database: AppDatabase) {
-    fun addGoal(goal: Goal): Completable {
+    fun addGoal(goal: Goal): Single<Long> {
+        var goalId = -1L
+
         with(goal) {
             database.runInTransaction({
                 val updatedSum = savings.sumByFloat { it.amount }
                 val copy = Goal(
-                    id,
-                    creationDate,
                     description,
                     cost,
+                    deadline,
+                    id,
+                    creationDate,
                     updatedSum,
                     remainingCost,
                     percentSaved,
-                    deadline,
                     daysUntilDeadline,
                     emphasizeRemainingDays,
                     suggestedSavingsPerDay,
@@ -28,17 +31,18 @@ class AddGoalUseCase @Inject constructor(private val database: AppDatabase) {
                     suggestedSavingsPerMonth,
                     savings)
 
-                database.getGoalRepository()
+                goalId = database.getGoalRepository()
                     .saveGoal(GoalMapper.toDataModel(copy))
 
-                val updatedSavings: Array<Savings> = savings
-                    .map { SavingsMapper.toDataModel(it) }.toTypedArray()
+                val updatedSavings: Array<Savings>? = savings.map { SavingsMapper.toDataModel(it) }.toTypedArray()
 
-                database.getSavingsRepository()
-                    .saveSavings(*updatedSavings)
+                updatedSavings?.let {
+                    database.getSavingsRepository()
+                        .saveSavings(*updatedSavings)
+                }
             })
         }
 
-        return Completable.complete()
+        return if (goalId != -1L) Single.just(goalId) else Single.error(Throwable(DATABASE_GENERIC_ERROR_MESSAGE))
     }
 }
