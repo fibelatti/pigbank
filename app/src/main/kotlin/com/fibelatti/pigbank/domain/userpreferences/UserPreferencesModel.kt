592be1adb2a2ebebb24a2bf5254fb5e871138941
Filipe Belatti <fibelatti@gmail.com>
2018-03-11 17:58:19 +0000

    Creates user preferences domain classes

---
+++ app/src/main/kotlin/com/fibelatti/pigbank/domain/userpreferences/UserPreferencesModel.kt
@@ -0,0 +1,6 @@
+package com.fibelatti.pigbank.domain.userpreferences
+
+data class UserPreferencesModel(
+    val analyticsEnabled: Boolean,
+    val crashReportsEnabled: Boolean
+)
---
+++ app/src/main/kotlin/com/fibelatti/pigbank/domain/userpreferences/UserPreferencesUseCase.kt
@@ -0,0 +1,28 @@
+package com.fibelatti.pigbank.domain.userpreferences
+
+import com.fibelatti.pigbank.data.userpreferences.UserPreferencesRepositoryContract
+import io.reactivex.Single
+import javax.inject.Inject
+
+class UserPreferencesUseCase @Inject constructor(
+    private val userPreferencesRepositoryContract: UserPreferencesRepositoryContract,
+    private val userPreferencesMapper: UserPreferencesMapper
+) {
+    fun getUserPreferences(): Single<UserPreferencesModel> =
+        userPreferencesRepositoryContract.getAllUserPreferences()
+            .map { userPreferencesMapper.toDomainModel(it) }
+
+    fun updateAnalyticsEnabled(value: Boolean): Single<Boolean> =
+        Single.create { emitter ->
+            val affectedRows = userPreferencesRepositoryContract.updateAnalyticsEnabled(value.toString())
+            if (affectedRows > 0) emitter.onSuccess(true) else emitter.onError(UserPreferencesError())
+        }
+
+    fun updateCrashReportsEnabled(value: Boolean): Single<Boolean> =
+        Single.create { emitter ->
+            val affectedRows = userPreferencesRepositoryContract.updateCrashReportsEnabled(value.toString())
+            if (affectedRows > 0) emitter.onSuccess(true) else emitter.onError(UserPreferencesError())
+        }
+}
+
+class UserPreferencesError : Throwable()
